a8eea2fae2e7d971e761cd7f0bcd1dfc4f6b7206
Harm van der Haas <harmvanderhaas@gmail.com>
2015-12-03 12:45:15 +0100

    Adding the foo and the bar

---
+++ TestRepo/TestRepo/FooFoo.cs
@@ -0,0 +1,15 @@
+﻿namespace TestRepo
+{
+    public class FooFoo
+    {
+        public string Foo()
+        {
+           return Bar("foo");
+        }
+
+        private string Bar(string foo)
+        {
+            return foo + "bar";
+        }
+    }
+}
